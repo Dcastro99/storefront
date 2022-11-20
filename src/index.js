a5c460ff1ca1c68ca4bd950b9c72261cac71d3e3
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-11-20 13:55:40 -0800

    strict mode

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
-  // </React.StrictMode>
 );
 
 
